6215bc87d49b94d97b0e496263cbe14adf6f58a8
AniketDohale <aniket.dohale.2801@gmail.com>
2023-09-17 18:55:10 +0530

    Second Stage

---
+++ Experiment_1/main_1.py
@@ -0,0 +1,85 @@
+from collections import Counter
+
+
+def get_Inputs(num_Values):
+    print("---------------------------------------")
+    values = []
+    for i in range(num_Values):
+        value = float(input(f"Enter {i+1} Value -> "))
+        values.append(value)
+    print("---------------------------------------")
+    return values
+
+
+def addition(values):
+    return sum(values)
+
+
+def mean(values):
+    return sum(values) / len(values)
+
+
+def median(values):
+    sorted_Values = sorted(values)
+    length = len(sorted_Values)
+
+    if length % 2 != 0:
+        return sorted_Values[length//2]
+    else:
+        mid_1 = sorted_Values[length//2 - 1]
+        mid_2 = sorted_Values[length//2]
+        return (mid_1 + mid_2)/2
+
+
+def mode(values):
+    number_Counts = Counter(values)
+    max_Frequency = max(number_Counts.values())
+    modes = [number for number, frequency in number_Counts.items()
+             if frequency == max_Frequency]
+
+    return modes
+
+
+while True:
+    print("---------------------------------------")
+    print("Choose Below Options :")
+    print("1. Addition")
+    print("2. Average")
+    print("3. Mean")
+    print("4. Median")
+    print("5. Mode")
+    print("X. Quit")
+    print("---------------------------------------")
+
+    choice = input("Choice -> ")
+
+    if choice == 'X' or choice == 'Quit':
+        print("Quitted..")
+        break
+
+    num_Values = int(input("Number of Values -> "))
+    values = get_Inputs(num_Values)
+
+    if choice == '1' or choice == 'Addition':
+        result = addition(values)
+        print("Addition ->", result)
+
+    elif choice == '2' or choice == 'Average':
+        result = mean(values)
+        print("Average ->", result)
+
+    elif choice == '3' or choice == 'Mean':
+        result = mean(values)
+        print("Mean ->", result)
+
+    elif choice == '4' or choice == 'Median':
+        result = median(values)
+        print("Median ->", result)
+
+    elif choice == '5' or choice == 'Mode':
+        result = mode(values)
+        print("Mode ->", result)
+
+    else:
+        print("---------------------------------------")
+        print("----- Invalid Input..Try Again.. ------")
